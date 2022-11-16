37f67abd7be03d702c450cdcb7bde34a25ef8438
Kathy <kathyaudrey2001@gmail.com>
2022-11-16 19:47:37 +0800

    add goal, add floor(plane)

--- Assets/Scripts/ItemCollect.cs
+++ Assets/Scripts/ItemCollect.cs
@@ -9,6 +9,7 @@ public class ItemCollect : MonoBehaviour
 	private ParticleSystem _particleSystem;
 	private AudioSource itemsound;
 	private Text _itemText;
+	private Transform _door;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class ItemCollect : MonoBehaviour
 		itemsound = GetComponent<AudioSource>();
 		_itemText = GameObject.Find("ItemCollectedText").GetComponent<Text>();
 		_itemText.text = amountOfItems + "/90";
+		_door = GameObject.Find("Door").GetComponent<Transform>();
     }
 
 	public void OnTriggerEnter(Collider Col){
@@ -27,8 +29,9 @@ public class ItemCollect : MonoBehaviour
 			_particleSystem.Play();
 			itemsound.Play();
 			_itemText.text = amountOfItems + "/90";
+			if (amountOfItems == 90) {_door.Translate(0, 10.0f, 0);}
+			
 		}
-	
 	}
 		
 
