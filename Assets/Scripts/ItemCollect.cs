e458e68a3d5632dc6e2ed1129a21abdc23b8b065
Kathy <kathyaudrey2001@gmail.com>
2022-11-16 18:46:35 +0800

    show no. of item collected in UI

--- Assets/Scripts/ItemCollect.cs
+++ Assets/Scripts/ItemCollect.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollect : MonoBehaviour
 {
 	public int amountOfItems;
 	private ParticleSystem _particleSystem;
 	private AudioSource itemsound;
+	private Text _itemText;
+
     // Start is called before the first frame update
     void Start()
     {
         _particleSystem = GetComponent<ParticleSystem>();
 		itemsound = GetComponent<AudioSource>();
+		_itemText = GameObject.Find("ItemCollectedText").GetComponent<Text>();
+		_itemText.text = amountOfItems + "/90";
     }
 
 	public void OnTriggerEnter(Collider Col){
@@ -21,6 +26,7 @@ public class ItemCollect : MonoBehaviour
 			Destroy(Col.gameObject);
 			_particleSystem.Play();
 			itemsound.Play();
+			_itemText.text = amountOfItems + "/90";
 		}
 	
 	}
